f7ef90c49af8a38000d1c00fef374c7aef5ba2a0
leonlowzd <e0407469@u.nus.edu>
2020-08-27 00:33:05 +0800

    Level 1. Greet, Echo, Exit
    Implement a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.
    
    Added another feature to remove spaces if user inputs "bye " or " bye ".

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
-        String sentence = "____________________________________________________________\n"
-                + "Hello! I'm Duke\n"
-                + "What can I do for you?\n"
-                + "____________________________________________________________\n"
-                + "Bye. Hope to see you again soon!\n"
-                + "____________________________________________________________";
-        System.out.println(sentence);
+        boolean exitFlag = false;
+        String toPrint = "____________________________________________________________\n"
+                + " Hello! I'm Duke\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+        System.out.println(toPrint);
+        while(!exitFlag){
+            String echoStatement;
+            Scanner in = new Scanner(System.in);
+            String line = in.nextLine();
+            String testExit = line.replace(" ","");
+            // Exit Condition, sets exitFlag to true
+            if(testExit.equals("bye")){
+                exitFlag = true;
+                echoStatement = "____________________________________________________________\n"
+                        + " Bye. Hope to see you again soon!\n"
+                        + "____________________________________________________________\n";
+            }
+            // Echo condition, exitFlag remains the same
+            else{
+                echoStatement = "____________________________________________________________\n"
+                        + line + "\n"
+                        + "____________________________________________________________\n";
+            }
+            System.out.println(echoStatement);
+        }
+
     }
 }
