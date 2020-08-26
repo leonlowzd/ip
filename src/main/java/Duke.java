0439f38fe5d856dd488d78675348a96bd7a0c725
leonlowzd <e0407469@u.nus.edu>
2020-08-27 00:35:29 +0800

    Level 3. Mark as Done
    Add the ability to mark tasks as done.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,12 +1,30 @@
 import java.util.Scanner;
 
 public class Duke {
+    public static class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        }
+
+        public void markAsDone(){
+            this.isDone = true;
+        }
+    }
+
     public static void main(String[] args) {
 
         boolean exitFlag = false;
-        int iterator = 0;
+        int index = 0;
         // Problem will come if there are more than 100 tasks
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         String toPrint = "____________________________________________________________\n"
                 + " Hello! I'm Duke\n"
                 + " What can I do for you?\n"
@@ -38,21 +56,32 @@ public class Duke {
                         int printOut = i+1;
                         sb.append(printOut);
                         sb.append(". ");
-                        sb.append(list[i]);
+                        sb.append("[");
+                        sb.append(list[i].getStatusIcon());
+                        sb.append("] ");
+                        sb.append(list[i].description);
                         sb.append("\n");
                     }
                 }
                 echoStatement = sb.toString();
             }
+            // Condition to mark task as completed
+            else if(testCondition.contains("done")){
+                int selectedIndex = Integer.parseInt(line.split(" ")[1])-1;
+                list[selectedIndex].markAsDone();
+                echoStatement = "Nice! I've marked this task as done: \n"
+                        +"["+ list[selectedIndex].getStatusIcon() +"] "+ list[selectedIndex].description + "\n"
+                        + "____________________________________________________________\n";
+            }
             // Echo condition, exitFlag remains the same
             else{
-
-                list[iterator] = line;
+                Task t = new Task(line);
+                list[index] = t;
 
                 echoStatement = "____________________________________________________________\n"
-                        + "added: "+ list[iterator] + "\n"
+                        + "added: "+ list[index].description + "\n"
                         + "____________________________________________________________\n";
-                iterator++;
+                index++;
             }
             System.out.println(echoStatement);
         }
