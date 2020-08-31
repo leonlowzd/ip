1a679d0b4dac0288d25165951c9896317f497671
leonlowzd <e0407469@u.nus.edu>
2020-09-01 01:08:47 +0800

    Level 4: ToDos, Events, Deadlines
    Added 3 types of Tasks and made Duke more robust: if user inputs something unrecognizable, programme will not crash

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,7 +14,7 @@ public class Duke {
             String line = in.nextLine();
             // In case user adds an extra space
             String testCondition = line.replaceAll(" ","");
-            System.out.println("testing: "+testCondition);
+//            System.out.println("testing: "+testCondition);
             if (testCondition.equals("bye")) {
                 exitFlag = true;
                 printByeMessage();
@@ -29,42 +29,50 @@ public class Duke {
                 else if (testCondition.contains("done")) {
                     printStatement = markTaskAsDone(list, line);
                 }
-                // Echo condition, exitFlag remains the same
+                // add task condition, exitFlag remains the same
                 else {
                     if (line.contains("todo")) {
+                        // remove todo indicator
                         line = line.replace("todo", "");
-                        ToDo td = new ToDo(line);
-                        list[index] = td;
-                        printStatement = printTaskDescription(index, list);
+                        ToDo task = new ToDo(line);
+                        list[index] = task;
                     } else if (line.contains("deadline")) {
-                        String deadlineDescription = line.replace("deadline", "");
-                        deadlineDescription = deadlineDescription.substring(0, deadlineDescription.lastIndexOf("/by"));
-                        String date = line.substring(line.lastIndexOf("/"));
-                        Deadline d = new Deadline(deadlineDescription,date);
-                        list[index] = d;
-                        printStatement = printTaskDescription(index, list);
-
+                        String deadlineDescription = extractDescriptionFromString("deadline",line);
+                        String date = extractDateFromString(line);
+                        Deadline task = new Deadline(deadlineDescription,date);
+                        list[index] = task;
+                    } else if (line.contains("event")) {
+                        String eventDescription = extractDescriptionFromString("event",line);
+                        String date = extractDateFromString(line);
+                        Event task = new Event(eventDescription,date);
+                        list[index] = task;
                     } else {
-                        String eventDescription = line.replace("event", "");
-                        eventDescription = eventDescription.substring(0, eventDescription.lastIndexOf("/at"));
-                        String date = line.substring(line.lastIndexOf("/"));
-                        Event e = new Event(eventDescription,date);
-                        list[index] = e;
-                        printStatement = printTaskDescription(index, list);
+                        // when user inputs something unrecognisable
+                        continue;
                     }
+                    printStatement = printTaskDescription(index, list);
                     index++;
                 }
                 System.out.println(printStatement);
             }
         }
     }
+    // This function removes the task type and extracts the description of the task
+    private static String extractDescriptionFromString(String type, String userInput){
+        String deadlineDescription = userInput.replace(type, "");
+        return deadlineDescription.substring(0, deadlineDescription.lastIndexOf("/"));
+    }
+    // This function extracts the date from the user input
+    private static String extractDateFromString(String userInput){
+        return userInput.substring(userInput.lastIndexOf("/"));
+    }
 
     private static String printTaskDescription(int index,  Task[] list){
         int numberOfTask = index+1;
         String printStatement = "____________________________________________________________\n"
                 + "Got it. I've added this task:\n"
-                +"     " + list[index] + "\n"
-                + "Now you have "+numberOfTask+" in the list .\n"
+                + "     " + list[index] + "\n"
+                + "Now you have "+numberOfTask+" in the list.\n"
                 + "____________________________________________________________\n";
         return printStatement;
     }
@@ -74,7 +82,7 @@ public class Duke {
         int selectedIndex = Integer.parseInt(line.split(" ")[1])-1;
         list[selectedIndex].markAsDone();
         String printStatement = "Nice! I've marked this task as done: \n"
-                + list[selectedIndex].getStatusIcon()+" "+ list[selectedIndex].description + "\n"
+                + list[selectedIndex] + "\n"
                 + "____________________________________________________________\n";
         return printStatement;
     }
